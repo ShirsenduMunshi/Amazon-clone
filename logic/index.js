4ff604896c9a3bc51870b892ef80fe73d81f6b55
ShirsenduMunshi <shirsendumunshi@gmail.com>
2023-07-27 23:29:02 +0530

    progress again

--- logic/index.js
+++ logic/index.js
@@ -1,3 +1,6 @@
+
+//dropdown card style
+
 function toggleBorder() {
     var divElement = document.getElementById("dropdown-menu-item");
 
@@ -29,7 +32,7 @@ function removeBorder(event) {
     }
 }
 
-
+//search input style
 
 function toggleBorderniput() {
     var divElement = document.getElementById("search-input");
@@ -62,3 +65,31 @@ function removeBorderall(event) {
         document.removeEventListener("click", removeBorderall);
     }
 }
+
+//search button style
+var hasBorder = false;
+
+function toggleBorderbtn() {
+    var divElement = document.querySelector(".serch-btn");
+
+    if (!hasBorder) {
+        // If no border is set, add a border to the div with the class "search-btn"
+        divElement.style.border = "4px solid #f3a847";
+        hasBorder = true;
+    } else {
+        // If a border is set, remove the border from the div with the class "search-btn"
+        divElement.style.border = "";
+        hasBorder = false;
+    }
+}
+
+// Add event listener to remove border when clicking anywhere on the document
+document.addEventListener("click", function (event) {
+    var divElement = document.querySelector(".serch-btn");
+    // Check if the click event occurred within the div
+    if (!divElement.contains(event.target)) {
+        // If click occurred outside the div, remove the border
+        divElement.style.border = "";
+        hasBorder = false;
+    }
+});
